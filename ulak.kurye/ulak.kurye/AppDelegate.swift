7cd3241c7d9341786c171e9b17d196e219659081
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2022-02-17 17:13:34 +0300

    Initial commit.

---
+++ ulak.kurye/ulak.kurye/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ulak.kurye
+//
+//  Created by Mehmet Karagöz on 17.02.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
