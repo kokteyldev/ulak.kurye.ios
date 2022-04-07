2c879e8f001bfc211bfd40a15d80ac2b1b4f7c82
melihcuneyterr <melih.cuneyter@kokteyl.com>
2022-04-07 10:20:30 +0300

    alertview added.

---
+++ ulak.kurye/ulak.kurye/Views/CustomAlertView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomAlertView.swift
+//  ulak.kurye
+//
+//  Created by Melih Cüneyter on 6.04.2022.
+//
+
+import UIKit
+
+class CustomAlertView: BaseVC {
+
+    @IBOutlet weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    // MARK: - Init
+    static func policyVC(_ orderTVC: OrderTVC, order: Order) -> CustomAlertView {
+        let vc = CustomAlertView(nibName: "CustomAlertView", bundle: nil)
+        vc.modalPresentationStyle = .overFullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        
+        return vc
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+        orderTVC.stopLoading()
+    }
+    
+    @IBAction func acceptTapped(_ sender: Any) {
+        getOrderFromPool(orderTVC, order: order)
+    }
+}
